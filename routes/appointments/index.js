640659d241ae91cea660b7169758e790323ae6ba
SWiessler <s.wiessler@comcast.net>
2021-12-06 12:05:16 -0500

    [BUILD] add appointments page

--- routes/appointments/index.js
+++ routes/appointments/index.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const router = new express.Router();
+// const apptData = require('../../data/Appointment');
 
 router.get('/', async (req, res) => {
-  res.json('all appointments');
+  const buildingsWithAccessGroups = await Building.find({accessGroup: {$in: accessGroups}});
+  const results = [];
+  for (let i = 0; i < buildingsWithAccessGroups.length; i++) {
+    const building = buildingsWithAccessGroups[i];
+    const newObj = {};
+    newObj._id = building._id.toString();
+    newObj.name = building.name.toString();
+    results.push(newObj);
+  }
+  res.render('appointments', {buildings: results});
 });
 
+router.post('/', async (req, res) => {
+  console.log("here");
+  console.log(res.body);
+  res.json('posted appointment form');
+})
 router.delete('/:id', async (req, res) => {
   const appointmentId = req.params.id;
   res.json(`deleted the appointment with id: ${appointmentId}`);
